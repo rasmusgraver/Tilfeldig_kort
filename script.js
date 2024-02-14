3e4621ff661f82e2258d71dcd452ae6c2b504b18
Rasmus Graver <rasmus.graver@gmail.com>
2024-02-14 12:57:12 +0100

    Velger også tilfeldig type av kort

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const kortElement = document.querySelector("img")
 
 // Tilfeldig tall mellom 1 og 13:
 const tall = Math.floor(Math.random()*13) + 1
-kortElement.src = "kortstokk/" + tall + "_of_spades.png"
+
+const types = ["hearts", "spades", "clubs", "diamonds"]
+// Tilfeldig tall mellom 0 og 3:
+const tilf_type = Math.floor(Math.random() * 4)
+const type = types[tilf_type]
+kortElement.src = "kortstokk/" + tall + "_of_" + type + ".png"
+
+
 
 
